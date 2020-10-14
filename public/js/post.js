7d9e5238148de534100bd968c6d8b181fd4111d8
Drozerah <drozerah@gmail.com>
2020-10-14 12:52:51 +0200

    refactor: apply eslint

--- public/js/post.js
+++ public/js/post.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-undef */
+
 // Copyright (c) 2020 Thomas G. drozerah@gmail.com
 // All rights reserved.
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-const defaultText = 
+const defaultText =
 `# This is a Markdown title
 
 > This is a bloc code
@@ -17,9 +19,8 @@ This is a paragraph
 
 document.forms[0].elements[0].value = defaultText
 
-
 /**
- * add event on form submit 
+ * add event on form submit
  */
 document.forms[0].elements[2].addEventListener('click', (e) => {
   e.preventDefault()
@@ -27,26 +28,25 @@ document.forms[0].elements[2].addEventListener('click', (e) => {
 })
 
 /**
- * Handle file download 
+ * Handle file download
  */
-function download(){
+function download () {
   const fileName = document.forms[0].elements[1].value
-  if(!fileName){
+  if (!fileName) {
     alert('Please enter a file mane!') // !DEBUG
   } else if (!validator.isAlphanumeric(fileName)) {
     alert('File name contains only letters and numbers!')
   } else {
     let text = document.forms[0].elements[0].value
-    text = text.replace(/\n/g, "\r\n") // retain the Line breaks
-    const blob = new Blob([text], { type: "text/plain"})
-    const anchor = document.createElement("a")
-    anchor.download =  `${fileName.trim()}.md` // get filemane
+    text = text.replace(/\n/g, '\r\n') // retain the Line breaks
+    const blob = new Blob([text], { type: 'text/plain' })
+    const anchor = document.createElement('a')
+    anchor.download = `${fileName.trim()}.md` // get filemane
     anchor.href = window.URL.createObjectURL(blob)
-    anchor.target ="_blank"
-    anchor.style.display = "none" // just to be safe!
+    anchor.target = '_blank'
+    anchor.style.display = 'none' // just to be safe!
     document.body.appendChild(anchor)
     anchor.click()
     document.body.removeChild(anchor)
   }
 }
-
